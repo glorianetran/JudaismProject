49c1a294bf9acf18a80392961fe8e86e522ec032
Gloriane Tran <gloriane@Glorianes-MacBook-Pro.local>
2018-04-20 13:11:04 -0700

    gestures

--- app/src/main/java/com/judaismproject/gloriane/judaismproject/fragment/MangaFragment.java
+++ app/src/main/java/com/judaismproject/gloriane/judaismproject/fragment/MangaFragment.java
@@ -1,108 +1,83 @@
 package com.judaismproject.gloriane.judaismproject.fragment;
 
-import android.net.Uri;
 import android.os.Bundle;
 import android.support.v4.app.Fragment;
+import android.view.GestureDetector;
 import android.view.LayoutInflater;
+import android.view.MotionEvent;
 import android.view.View;
 import android.view.ViewGroup;
-
+import android.widget.ImageView;
+import android.widget.Toast;
 import com.judaismproject.gloriane.judaismproject.R;
 
-/**
- * A simple {@link Fragment} subclass.
- * Activities that contain this fragment must implement the
- * {@link MangaFragment.OnFragmentInteractionListener} interface
- * to handle interaction events.
- * Use the {@link MangaFragment#newInstance} factory method to
- * create an instance of this fragment.
- */
-public class MangaFragment extends Fragment {
-    // TODO: Rename parameter arguments, choose names that match
-    // the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
-    private static final String ARG_PARAM1 = "param1";
-    private static final String ARG_PARAM2 = "param2";
+import java.util.ArrayList;
 
-    // TODO: Rename and change types of parameters
-    private String mParam1;
-    private String mParam2;
+public class MangaFragment extends Fragment{
 
-    private OnFragmentInteractionListener mListener;
+    ImageView imageView;
 
-    public MangaFragment() {
-        // Required empty public constructor
-    }
+    ArrayList imageIds;
 
-    /**
-     * Use this factory method to create a new instance of
-     * this fragment using the provided parameters.
-     *
-     * @param param1 Parameter 1.
-     * @param param2 Parameter 2.
-     * @return A new instance of fragment MangaFragment.
-     */
-    // TODO: Rename and change types and number of parameters
-    public static MangaFragment newInstance(String param1, String param2) {
-        MangaFragment fragment = new MangaFragment();
-        Bundle args = new Bundle();
-        args.putString(ARG_PARAM1, param1);
-        args.putString(ARG_PARAM2, param2);
-        fragment.setArguments(args);
-        return fragment;
-    }
+    private static final int SWIPE_DISTANCE_THRESHOLD = 100;
+    private static final int SWIPE_VELOCITY_THRESHOLD = 100;
+
+
+    public MangaFragment(){}
 
     @Override
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        if (getArguments() != null) {
-            mParam1 = getArguments().getString(ARG_PARAM1);
-            mParam2 = getArguments().getString(ARG_PARAM2);
-        }
     }
 
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_manga, container, false);
+        View view =  inflater.inflate(R.layout.fragment_manga, container, false);
+        imageView = view.findViewById(R.id.image);
+
+        final GestureDetector gesture = new GestureDetector(getActivity(),
+                new GestureDetector.SimpleOnGestureListener() {
+
+                    @Override
+                    public boolean onDown(MotionEvent e) {
+                        return true;
+                    }
+
+                    @Override
+                    public boolean onFling(MotionEvent e1, MotionEvent e2, float velocityX,
+                                           float velocityY) {
+                        float distanceX = e2.getX() - e1.getX();
+                        float distanceY = e2.getY() - e1.getY();
+                        if (Math.abs(distanceX) > Math.abs(distanceY) && Math.abs(distanceX) > SWIPE_DISTANCE_THRESHOLD && Math.abs(velocityX) > SWIPE_VELOCITY_THRESHOLD) {
+                            if (distanceX > 0)
+                                onSwipeRight();
+                            else
+                                onSwipeLeft();
+                            return true;
+                        }
+                        return false;
+                    }
+
+                    public void onSwipeLeft() {
+                        Toast.makeText(getActivity(), "left", Toast.LENGTH_SHORT).show();
+
+                    }
+
+                    public void onSwipeRight() {
+                        Toast.makeText(getActivity(), "right", Toast.LENGTH_SHORT).show();
+                    }
+        });
+
+        view.setOnTouchListener(new View.OnTouchListener() {
+            @Override
+            public boolean onTouch(View v, MotionEvent event) {
+                return gesture.onTouchEvent(event);
+            }
+        });
+
+        return view;
     }
 
-    // TODO: Rename method, update argument and hook method into UI event
-    public void onButtonPressed(Uri uri) {
-        if (mListener != null) {
-            mListener.onFragmentInteraction(uri);
-        }
-    }
-
-//    @Override
-//    public void onAttach(Context context) {
-//        super.onAttach(context);
-//        if (context instanceof OnFragmentInteractionListener) {
-//            mListener = (OnFragmentInteractionListener) context;
-//        } else {
-//            throw new RuntimeException(context.toString()
-//                    + " must implement OnFragmentInteractionListener");
-//        }
-//    }
-
-    @Override
-    public void onDetach() {
-        super.onDetach();
-        mListener = null;
-    }
-
-    /**
-     * This interface must be implemented by activities that contain this
-     * fragment to allow an interaction in this fragment to be communicated
-     * to the activity and potentially other fragments contained in that
-     * activity.
-     * <p>
-     * See the Android Training lesson <a href=
-     * "http://developer.android.com/training/basics/fragments/communicating.html"
-     * >Communicating with Other Fragments</a> for more information.
-     */
-    public interface OnFragmentInteractionListener {
-        // TODO: Update argument type and name
-        void onFragmentInteraction(Uri uri);
-    }
 }
